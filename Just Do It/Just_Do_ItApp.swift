55c70f945a0931dd5a3c5d637eb579dc6bb17ebb
Stevie <75964167+Stevie-Bland@users.noreply.github.com>
2023-03-16 10:47:34 -0500

    Initial Commit

---
+++ Just Do It/Just_Do_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_Do_ItApp.swift
+//  Just Do It
+//
+//  Created by Steven on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Just_Do_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
